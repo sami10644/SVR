34b00c4ef3f6b7b9d9a11833617f5a37c21a6d35
sami10644 <61244442+sami10644@users.noreply.github.com>
2021-03-11 19:37:50 +0600

    Update support_vector_regression.py

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
